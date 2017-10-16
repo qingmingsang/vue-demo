788c800e3bdd0b6227d8977d857c20021df304c8
qingming <358242939@qq.com>
2017-10-16 09:32:27 +0800

    update

---
+++ 2.x/webpack/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: [
+                    'vue-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    resolve: { extensions: [".vue", ".js"] },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'vue test',
+            favicon: './favicon.ico',
+            template: 'template.html'
+        })
+    ],
+    devServer: {
+        hot: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 7000
+    },
+    devtool: "eval"
+};
