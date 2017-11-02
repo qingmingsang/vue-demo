3eb9596c91078a31a05c00fdf21c163f259144b7
qingming <358242939@qq.com>
2017-11-02 19:43:59 +0800

    update

---
+++ 2.x/simple-todo-app/src/components/BaseInputText.vue
@@ -0,0 +1,30 @@
+<template>
+  <input
+    type="text"
+    class="input"
+    :value="value"
+    v-on="$listeners"
+    @input="$emit('input', $event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../variables.scss';
+
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid $vue-blue;
+}
+</style>
