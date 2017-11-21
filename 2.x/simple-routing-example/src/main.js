eb0932c16e4fd98fe1fdff84b7567759c74abe62
qingming <358242939@qq.com>
2017-11-21 10:51:04 +0800

    update

---
+++ 2.x/simple-routing-example/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import routes from './routes'
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent() {
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/404.vue')
+    }
+  },
+  render(h) {
+    return h(this.ViewComponent)
+  }
+})
+
+window.addEventListener('popstate', () => {
+  console.log('触发popstate')
+  app.currentRoute = window.location.pathname
+})
